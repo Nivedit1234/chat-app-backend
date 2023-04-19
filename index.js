659d838831fcce28e2e79e2ce31d94f0f99705c3
Nivedit1234 <75367309+Nivedit1234@users.noreply.github.com>
2023-04-20 03:12:55 +0530

    project done

---
+++ index.js
@@ -0,0 +1,24 @@
+const express = require('express');
+const cors = require('cors');
+const axios = require('axios');
+
+const app = express();
+app.use(express.json());
+app.use(cors({ origin: true }));
+
+app.post('/authenticate', async (req, res) => {
+  const { username } = req.body;
+  // Get or create user on Chat Engine!
+  try {
+    const r = await axios.put(
+      'https://api.chatengine.io/users/',
+      { username: username, secret: username, first_name: username },
+      { headers: { 'Private-Key': '0d8eeff3-6691-4ff2-9ec1-8e5855b71787' } }
+    );
+    return res.status(r.status).json(r.data);
+  } catch (e) {
+    return res.status(e.status).json(e.data);
+  }
+});
+
+app.listen(3001);
